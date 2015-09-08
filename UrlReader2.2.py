0e6f10831cf569ec622dc064c7d69637da19d79e
NULLBOT8 <nathanward1620523@gmail.com>
2015-09-07 21:00:39 -0400

    Create UrlReader2.2.py
    In this version the output is colored and it prompts the user if he/she wants to add another url.

---
+++ UrlReader2.2.py
@@ -0,0 +1,23 @@
+# +++++++++++++++UrlReader++++++++++++++++
+# This script takes a url as an input and prints its source code
+# AGAIN: this code is very basic.
+
+import urllib
+
+urls = []
+html_text = urllib.urlopen(raw_input("Type a url: "))
+urls.append(html_text)
+print html_text.read()
+
+again = (raw_input("Would you like to analyze another url? Y or N?: "))
+if again == 'Y':
+    while len(urls) < 5:
+        another_url = urllib.urlopen(raw_input("Type another url: "))
+        urls.append(another_url)
+        print another_url.read()
+        print '\033[1;44m---------------------------------------------------------------------\033[1;m'
+        print ("Urls entered: " + str(len(urls)))
+
+if len(urls) == 5:
+    print '\033[1;36mALL URLS HAVE BEEN ANALYZED\033[1;m'
+    print '\033[1;44m---------------------------------------------------------------------\033[1;m'
